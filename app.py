fa17083f0549c725d90cae3533625d57cfcfc335
Michal Rutkowski <rutkowski.michal.jp@gmail.com>
2019-11-01 13:08:20 +0900

    * Hooking up sample app code

--- app.py
+++ app.py
@@ -14,10 +14,25 @@
 import os
 import sys
 
+import package.app.converter as Converter
 import package.app.iplugin as PluginRegistry
 
 if __name__ == '__main__':
+    current_dir = os.path.dirname(sys.argv[0])
+    if len(sys.argv) < 3:
+        raise IOError("Please provide an input file and output format")
+
+    input_file = os.path.abspath(sys.argv[1])
+    if not os.path.isfile(input_file):
+        raise IOError("Sorry this input file is invalid.")
+
     # Load plugins from package/plugins directory
-    mydir = os.path.dirname(sys.argv[0])
-    plugins = PluginRegistry.discover_plugins([os.path.join(mydir, "package", "plugins")])
-    print(plugins)
+    print("Loading plugins..")
+    plugins = PluginRegistry.discover_plugins([os.path.join(current_dir, "package", "plugins")])
+    #print(plugins)
+
+    # Instance our converter
+    print("Loading media converter..")
+    converter = Converter.Converter(input_file, plugins)
+    converter.set_output_format(sys.argv[2])
+    converter.encode()
