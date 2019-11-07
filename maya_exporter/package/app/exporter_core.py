51515fcfd413c72f2b75842a40648607ba565449
Michal Rutkowski <rutkowski.michal.jp@gmail.com>
2019-11-07 13:23:58 +0900

    * Added error handling to ExporterCore

--- maya_exporter/package/app/exporter_core.py
+++ maya_exporter/package/app/exporter_core.py
@@ -20,6 +20,7 @@ import sys
 
 # python 2.7
 import iplugin27 as PluginRegistry
+import maya.cmds as cmds
 
 
 class ExporterCore():
@@ -46,8 +47,34 @@ class ExporterCore():
             Tries to find matching export routine for maya_object
             based on name prefix, then calls that routine if found.
         """
-        processing_method = self._get_processing_hook(maya_object)
-        processing_method(maya_object)
+        plugin_name, processing_method = self._get_processing_hook(maya_object)
+
+        # handle error when object name is not supported
+        if processing_method is None:
+            prefixes_dict = self._get_all_prefixes()
+            error_msg = "Please rename your object to use one of supported plugin prefixes:\n"
+            error_msg += str(prefixes_dict).replace("{", "").replace("}", "").replace("'", "").replace(", ", "\n")
+            cmds.confirmDialog(
+                title="Maya Exporter",
+                message=error_msg,
+                button=["Cancel"],
+                defaultButton="Cancel")
+            return
+
+        # confirom dialog before exporting
+        dialog_msg = "Would you like to export " + maya_object + " using " + plugin_name + "?"
+        confirm_button = "OK"
+        result = cmds.confirmDialog(
+            title="Maya Exporter",
+            message=dialog_msg,
+            button=[confirm_button, "Cancel"],
+            defaultButton=confirm_button,
+            cancelButton="Cancel",
+            dismissString="Cancel")
+
+        # if everything is ok run export routine
+        if(result == confirm_button):
+            processing_method(maya_object)
 
     def _get_processing_hook(self, maya_object):
         """
@@ -55,11 +82,21 @@ class ExporterCore():
             Takes input filename and returns hook if found.
             Note that name matching is handled by plugins.
         """
-        #  Plugin usage #
         # Plugins are classes - we need to instantiate them to get objects.
         plugins = [P(maya_object) for P in self.plugins]
         for p in plugins:
             hook = p.get_export_hook(maya_object)
             if hook:
-                return hook
-        raise RuntimeError("Sorry could not find appropriate plugin")
+                return p.plugin_name, hook
+        return None, None
+
+    def _get_all_prefixes(self):
+        """
+            Scans plugins and returns all supported object prefices.
+        """
+        # Plugins are classes - we need to instantiate them to get objects.
+        plugins = [P("default") for P in self.plugins]
+        prefix_dict = {}
+        for p in plugins:
+            prefix_dict[p.plugin_name] = p.get_prefix()
+        return prefix_dict
