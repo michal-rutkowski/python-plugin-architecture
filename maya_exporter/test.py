6479936237620a86e6a8815ad0b46f3fc0eea409
Michal Rutkowski <rutkowski.michal.jp@gmail.com>
2019-11-07 13:25:48 +0900

    * Updated test file

--- maya_exporter/test.py
+++ maya_exporter/test.py
@@ -1,10 +1,19 @@
+# -*- Coding: utf-8 -*-
+#!/usr/bin/python
+
 import sys
 import maya.cmds as cmds
 
+# Read script from local path (or copy to maya2019/Scripts)
 path = "C:/rumichal/Git/PythonPluginSetup/maya_exporter"
 if path not in sys.path:
-    sys.path.append("C:/rumichal/Git/PythonPluginSetup/maya_exporter")
-from maya_exporter import maya_exporter
+    sys.path.append(path)
 
-sel = cmds.ls(sl=True)
-maya_exporter.run(sel[0])
+# Import main module
+import maya_exporter
+
+# Get first selected object in Maya
+sel = cmds.ls(sl=True)[0]
+
+# Export selection
+maya_exporter.run(sel)
